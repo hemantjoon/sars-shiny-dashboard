0c3b48d9dfcb9270e6cde483b861aaef09058d73
Hemant Kumar Joon <rockstarhemantjoon@gmail.com>
2024-04-25 11:34:08 +0200

    merged codebase into app.R

--- functions.R
+++
@@ -1,9 +0,0 @@
-
-format_bignum = function(n){
-  case_when(
-    n >= 1e12 ~ paste(round(n/1e12, digits = 2), 'Tn'),
-    n >= 1e9 ~ paste(round(n/1e9, digits = 2), 'Bn'),
-    n >= 1e6 ~ paste(round(n/1e6, digits = 2), 'M'),
-    n >= 1e3 ~ paste(round(n/1e3, digits = 2), 'K'),
-    TRUE ~ as.character(n))
-}
